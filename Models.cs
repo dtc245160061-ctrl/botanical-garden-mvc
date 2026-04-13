9ad0f5d752c76003b823597b9fa16d94aec4b1d6
Sơn Đẹp Trai <dtc245160061@ictu.edu.vn>
2026-04-13 17:47:36 +0700

    Create Models.cs

---
+++ Models.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GardenApp.Models
+{
+    public class Plant
+    {
+        public int PlantId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public double Height { get; set; }
+    }
+}
