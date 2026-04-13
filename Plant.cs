f668e56ada2fa2b7a9e0d6d3763848f61a21f145
Sơn Đẹp Trai <dtc245160061@ictu.edu.vn>
2026-04-13 17:54:33 +0700

    Update Plant.cs

--- Plant.cs
+++ Plant.cs
@@ -1,22 +1,13 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GardenApp.Models
+namespace GardenApp
 {
     public class Plant
     {
         public int PlantId { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
         public string Species { get; set; }
-
         public double Height { get; set; }
 
         public int SectionId { get; set; }
-
-        [ForeignKey("SectionId")]
         public Section Section { get; set; }
     }
 }
