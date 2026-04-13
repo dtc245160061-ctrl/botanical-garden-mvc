0b3da2b13e59dc4f9618dea530c7d3a06a69b4c1
Sơn Đẹp Trai <dtc245160061@ictu.edu.vn>
2026-04-13 18:00:54 +0700

    Update Section.cs

--- Section.cs
+++ Section.cs
@@ -6,8 +6,6 @@ namespace GardenApp
     {
         public int SectionId { get; set; }
         public string Name { get; set; }
-        public string Climate { get; set; }
-        public double Size { get; set; }
 
         public List<Plant> Plants { get; set; }
     }
