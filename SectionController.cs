121366bb6cbef35a21d59420b3dfc576a0ff2bac
Sơn Đẹp Trai <dtc245160061@ictu.edu.vn>
2026-04-13 18:01:58 +0700

    Update SectionController.cs

--- SectionController.cs
+++ SectionController.cs
@@ -1,25 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace GardenApp
 {
     public class SectionController : Controller
     {
-        private readonly GardenContext _context;
-
-        public SectionController(GardenContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
-            var data = _context.Sections
-                .Include(s => s.Plants)
-                .ToList();
+            var sections = new List<Section>
+            {
+                new Section
+                {
+                    SectionId = 1,
+                    Name = "Tropical",
+                    Plants = new List<Plant>
+                    {
+                        new Plant { PlantId = 1, Name = "Monstera", Species = "Monstera Deliciosa" },
+                        new Plant { PlantId = 2, Name = "Banana", Species = "Musa" }
+                    }
+                },
+                new Section
+                {
+                    SectionId = 2,
+                    Name = "Desert",
+                    Plants = new List<Plant>
+                    {
+                        new Plant { PlantId = 3, Name = "Cactus", Species = "Cactaceae" }
+                    }
+                }
+            };
 
-            return View(data);
+            return View(sections);
         }
     }
 }
